fb78eada4a81d6fc7f685fd1287169f4a56ae506
Yosef <yosefplayz4580@gmail.com>
2023-03-14 17:33:11 +0200

    did smth as well

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ function App() {
   return (
     <>
       <h1 style={{color: 'white'}}>Hey this is something new</h1>
+      <p>Decided adding something else as well lol</p>
+      <p>hey</p>
     </>
   )
 }
